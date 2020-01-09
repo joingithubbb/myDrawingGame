cb13732c3d1482ef4e0271b98f520af72fa4faee
53091601 <florian.haenny@icr.ch>
2020-01-09 14:42:39 +0100

    Updates

---
+++ precache-manifest.b44a8f993033fbdb6a071f1a773de137.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d70d4602c51b0b880984c390133e36c8",
+    "url": "/myDrawingGame/index.html"
+  },
+  {
+    "revision": "e1e7c0b8d8cbe161e1d9",
+    "url": "/myDrawingGame/static/css/main.6fdd7596.chunk.css"
+  },
+  {
+    "revision": "8fa6eb2f55334851fa49",
+    "url": "/myDrawingGame/static/js/2.76dc1547.chunk.js"
+  },
+  {
+    "revision": "a244fffae27d60e5a79fcf3e76cfa315",
+    "url": "/myDrawingGame/static/js/2.76dc1547.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e1e7c0b8d8cbe161e1d9",
+    "url": "/myDrawingGame/static/js/main.33fa2c2f.chunk.js"
+  },
+  {
+    "revision": "4bb2fbac038852edb3fd",
+    "url": "/myDrawingGame/static/js/runtime-main.04272628.js"
+  }
+]);
